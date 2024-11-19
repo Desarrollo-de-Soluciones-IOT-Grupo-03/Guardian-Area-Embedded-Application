88a680bc5477b82e37b6a6b89831dce16162312c
steve <stev8855@gmail.com>
2024-11-18 23:46:52 -0500

    feat: add Constants.cpp for device configuration and API key management

---
+++ Constants.cpp
@@ -0,0 +1,5 @@
+#include "Constants.h"
+
+const char* deviceUUID = "9479d017-d916-486d-8644-ee178a3d60f9";  // GuardianArea Device UUID
+const char* apiKeyFilePath = "api_key.txt";                       // File path containing the API Key required to send data to the server
+const unsigned long gpsUpdateInterval = 1000;                     // How frequent GPS location data should be sent to the server in ms
